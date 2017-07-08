cccb2ecec1d1b3c5c8633d74a02b4349f7c78f05
zys <jbjyz314@126.com>
2017-07-08 20:28:08 +0800

    xp

--- js/base.js
+++ js/base.js
@@ -1,6 +1,6 @@
 Date.prototype.format = function(formatStr){
     var str = formatStr;
-    var Week = ['æ—¥', 'ä¸€', 'äºŒ', 'ä¸‰', 'å››', 'äº”', 'å…­'];
+    var Week = ['ÈÕ', 'Ò»', '¶þ', 'Èý', 'ËÄ', 'Îå', 'Áù'];
 
     str = str.replace(/yyyy|YYYY/, this.getFullYear());
     str = str.replace(/yy|YY/, (this.getYear() % 100) > 9 ? (this.getYear() % 100).toString() : '0' + (this.getYear() % 100));
@@ -24,13 +24,13 @@ Date.prototype.format = function(formatStr){
 }
 
 $.fn.datetimepicker.dates['zh'] = {  
-                days:       ["æ˜ŸæœŸæ—¥", "æ˜ŸæœŸä¸€", "æ˜ŸæœŸäºŒ", "æ˜ŸæœŸä¸‰", "æ˜ŸæœŸå››", "æ˜ŸæœŸäº”", "æ˜ŸæœŸå…­","æ˜ŸæœŸæ—¥"],  
-                daysShort:  ["æ—¥", "ä¸€", "äºŒ", "ä¸‰", "å››", "äº”", "å…­","æ—¥"],  
-                daysMin:    ["æ—¥", "ä¸€", "äºŒ", "ä¸‰", "å››", "äº”", "å…­","æ—¥"],  
-                months:     ["ä¸€æœˆ", "äºŒæœˆ", "ä¸‰æœˆ", "å››æœˆ", "äº”æœˆ", "å…­æœˆ", "ä¸ƒæœˆ", "å…«æœˆ", "ä¹æœˆ", "åæœˆ", "åä¸€æœˆ","åäºŒæœˆ"],  
-                monthsShort:  ["ä¸€", "äºŒ", "ä¸‰", "å››", "äº”", "å…­", "ä¸ƒ", "å…«", "ä¹", "å", "åä¸€", "åäºŒ"],  
-                meridiem:    ["ä¸Šåˆ", "ä¸‹åˆ"],   
-                today:       "ä»Šå¤©"  
+                days:       ["ÐÇÆÚÈÕ", "ÐÇÆÚÒ»", "ÐÇÆÚ¶þ", "ÐÇÆÚÈý", "ÐÇÆÚËÄ", "ÐÇÆÚÎå", "ÐÇÆÚÁù","ÐÇÆÚÈÕ"],  
+                daysShort:  ["ÈÕ", "Ò»", "¶þ", "Èý", "ËÄ", "Îå", "Áù","ÈÕ"],  
+                daysMin:    ["ÈÕ", "Ò»", "¶þ", "Èý", "ËÄ", "Îå", "Áù","ÈÕ"],  
+                months:     ["Ò»ÔÂ", "¶þÔÂ", "ÈýÔÂ", "ËÄÔÂ", "ÎåÔÂ", "ÁùÔÂ", "ÆßÔÂ", "°ËÔÂ", "¾ÅÔÂ", "Ê®ÔÂ", "Ê®Ò»ÔÂ","Ê®¶þÔÂ"],  
+                monthsShort:  ["Ò»", "¶þ", "Èý", "ËÄ", "Îå", "Áù", "Æß", "°Ë", "¾Å", "Ê®", "Ê®Ò»", "Ê®¶þ"],  
+                meridiem:    ["ÉÏÎç", "ÏÂÎç"],   
+                today:       "½ñÌì"  
         };
 
 
@@ -39,20 +39,20 @@ window.Util = {
      * Created with JetBrains WebStorm.
      * User: liyong.wang
      * Date: 16/12/5
-     * Time: ä¸‹åˆ4:56
-     * Desc: å„ç§æ—¥æœŸæ ¼å¼åŒ–å‡½æ•°
+     * Time: ÏÂÎç4:56
+     * Desc: ¸÷ÖÖÈÕÆÚ¸ñÊ½»¯º¯Êý
      */
     dateFormat:function(dateStr){
         return dateStr ? new Date(dateStr).format('yyyy-MM-dd hh:mm:ss') : "";
     },
     dateFormatWithChinese:function(dateStr){
-        return dateStr ? new Date(dateStr).format('yyyyå¹´MMæœˆddæ—¥ hh:mm:ss') : "";
+        return dateStr ? new Date(dateStr).format('yyyyÄêMMÔÂddÈÕ hh:mm:ss') : "";
     },
     dateFormatByMonth:function(dateStr){
         return dateStr ? new Date(dateStr).format('yyyy-MM-dd') : "";
     },
     dateFormatByMonthWithChinese:function(dateStr){
-        return dateStr ? new Date(dateStr).format('yyyyå¹´MMæœˆddæ—¥') : "";
+        return dateStr ? new Date(dateStr).format('yyyyÄêMMÔÂddÈÕ') : "";
     },
     dayFormat: function(dataStr, type){
         if(!dataStr){
@@ -119,5 +119,173 @@ $(function(){
         		$(".endDate[name='"+dateGroup+"']").datetimepicker('setStartDate',startDate);
 	        }
 	    });		
-	})
+	});
+
+    $(".select_component .checked_text").on("click",function(e){
+        e.stopPropagation();
+        $(this).next(".select_list").toggle();
+    });
+    $(".select_component .select_list li").on("click",function(e){
+        e.stopPropagation();
+        var $parent = $(this).parent();
+        var $parents = $parent.parent();
+        var checkedVal = $(this).data('value');
+        var checkedText = $(this).text();
+        $parent.toggle();
+
+        $parents.find(".checked_text").text(checkedText);
+        if(checkedVal == -1){
+            $parents.find('input[type="hidden"]').val('');
+        }else{
+            $parents.find('input[type="hidden"]').val(checkedVal);
+        }
+    });
+    $(document).on("click",function(){
+        $(".select_component .select_list").hide();
+    })
+})Date.prototype.format = function(formatStr){
+    var str = formatStr;
+    var Week = ['ÈÕ', 'Ò»', '¶þ', 'Èý', 'ËÄ', 'Îå', 'Áù'];
+
+    str = str.replace(/yyyy|YYYY/, this.getFullYear());
+    str = str.replace(/yy|YY/, (this.getYear() % 100) > 9 ? (this.getYear() % 100).toString() : '0' + (this.getYear() % 100));
+    var month = this.getMonth() + 1;
+    str = str.replace(/MM/, month > 9 ? month.toString() : '0' + month);
+    str = str.replace(/M/g, month);
+
+    str = str.replace(/w|W/g, Week[this.getDay()]);
+
+    str = str.replace(/dd|DD/, this.getDate() > 9 ? this.getDate().toString() : '0' + this.getDate());
+    str = str.replace(/d|D/g, this.getDate());
+
+    str = str.replace(/hh|HH/, this.getHours() > 9 ? this.getHours().toString() : '0' + this.getHours());
+    str = str.replace(/h|H/g, this.getHours());
+    str = str.replace(/mm/, this.getMinutes() > 9 ? this.getMinutes().toString() : '0' + this.getMinutes());
+    str = str.replace(/m/g, this.getMinutes());
+
+    str = str.replace(/ss|SS/, this.getSeconds() > 9 ? this.getSeconds().toString() : '0' + this.getSeconds());
+    str = str.replace(/s|S/g, this.getSeconds());
+    return str;
+}
+
+$.fn.datetimepicker.dates['zh'] = {  
+                days:       ["ÐÇÆÚÈÕ", "ÐÇÆÚÒ»", "ÐÇÆÚ¶þ", "ÐÇÆÚÈý", "ÐÇÆÚËÄ", "ÐÇÆÚÎå", "ÐÇÆÚÁù","ÐÇÆÚÈÕ"],  
+                daysShort:  ["ÈÕ", "Ò»", "¶þ", "Èý", "ËÄ", "Îå", "Áù","ÈÕ"],  
+                daysMin:    ["ÈÕ", "Ò»", "¶þ", "Èý", "ËÄ", "Îå", "Áù","ÈÕ"],  
+                months:     ["Ò»ÔÂ", "¶þÔÂ", "ÈýÔÂ", "ËÄÔÂ", "ÎåÔÂ", "ÁùÔÂ", "ÆßÔÂ", "°ËÔÂ", "¾ÅÔÂ", "Ê®ÔÂ", "Ê®Ò»ÔÂ","Ê®¶þÔÂ"],  
+                monthsShort:  ["Ò»", "¶þ", "Èý", "ËÄ", "Îå", "Áù", "Æß", "°Ë", "¾Å", "Ê®", "Ê®Ò»", "Ê®¶þ"],  
+                meridiem:    ["ÉÏÎç", "ÏÂÎç"],   
+                today:       "½ñÌì"  
+        };
+
+
+window.Util = {
+	    /**
+     * Created with JetBrains WebStorm.
+     * User: liyong.wang
+     * Date: 16/12/5
+     * Time: ÏÂÎç4:56
+     * Desc: ¸÷ÖÖÈÕÆÚ¸ñÊ½»¯º¯Êý
+     */
+    dateFormat:function(dateStr){
+        return dateStr ? new Date(dateStr).format('yyyy-MM-dd hh:mm:ss') : "";
+    },
+    dateFormatWithChinese:function(dateStr){
+        return dateStr ? new Date(dateStr).format('yyyyÄêMMÔÂddÈÕ hh:mm:ss') : "";
+    },
+    dateFormatByMonth:function(dateStr){
+        return dateStr ? new Date(dateStr).format('yyyy-MM-dd') : "";
+    },
+    dateFormatByMonthWithChinese:function(dateStr){
+        return dateStr ? new Date(dateStr).format('yyyyÄêMMÔÂddÈÕ') : "";
+    },
+    dayFormat: function(dataStr, type){
+        if(!dataStr){
+            return '';
+        }
+        if(!type){
+            type = '-';
+        }
+        var dateSs = new Date(dataStr);
+        var year = dateSs.getFullYear();
+        var month = dateSs.getMonth() + 1;
+        var day = dateSs.getDate();
+        month> 9 ? null : (month = '0' + month);
+        day > 9 ? null : (day = '0' + day);
+        return year + type + month + type + day;
+    },
+    clone:function(obj) {
+        var o;
+        if (typeof obj == "object") {
+            if (obj === null) {
+                o = null;
+            } else {
+                if (obj instanceof Array) {
+                    o = [];
+                    for (var i = 0, len = obj.length; i < len; i++) {
+                        o.push(this.clone(obj[i]));
+                    }
+                } else {
+                    o = {};
+                    for (var j in obj) {
+                        o[j] = this.clone(obj[j]);
+                    }
+                }
+            }
+        } else {
+            o = obj;
+        }
+        return o;
+    },
+}
+
+
+$(function(){
+	var newDate = new Date();
+
+	$(".date_picker").each(function(i){
+		var $this = $(this);
+		var setOption = {
+			language:  'zh',
+			format: 'yyyy-mm-dd',		
+			minView: "month",
+			todayBtn: true,			
+		}
+		if($this.hasClass('startDate') || $this.hasClass('endDate')){
+			setOption.endDate = new Date();
+		}
+
+		$this.datetimepicker(setOption).on('changeDate', function (ev) {  
+			var startDate = Util.dateFormatByMonth(ev.date);
+			var option = Util.clone(setOption);
+			var dateGroup = $(this).attr('name');
+	        $this.datetimepicker('hide');  
+	        if($this.hasClass('startDate')){
+        		$(".endDate[name='"+dateGroup+"']").datetimepicker('setStartDate',startDate);
+	        }
+	    });		
+	});
+
+    $(".select_component .checked_text").on("click",function(e){
+        e.stopPropagation();
+        $(this).next(".select_list").toggle();
+    });
+    $(".select_component .select_list li").on("click",function(e){
+        e.stopPropagation();
+        var $parent = $(this).parent();
+        var $parents = $parent.parent();
+        var checkedVal = $(this).data('value');
+        var checkedText = $(this).text();
+        $parent.toggle();
+
+        $parents.find(".checked_text").text(checkedText);
+        if(checkedVal == -1){
+            $parents.find('input[type="hidden"]').val('');
+        }else{
+            $parents.find('input[type="hidden"]').val(checkedVal);
+        }
+    });
+    $(document).on("click",function(){
+        $(".select_component .select_list").hide();
+    })
 })
